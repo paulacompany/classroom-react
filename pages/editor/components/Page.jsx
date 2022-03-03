0e5e86b2ca4a55695ad08f6114ba20f2a1813c5a
letterpaulsand <letterpaulsand@gmail.com>
2022-03-03 23:56:55 +0800

    editor finish

--- pages/editor/components/Page.jsx
+++ pages/editor/components/Page.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { tify } from "chinese-conv";
 
-export default function Page({setPages}){
+export default function Page({setPages, pages}){
 
     return(
         <div>
             <p className="d-block h2 m-3">頁數:</p>
-            <input type={'text'} className="m-4" placeholder="p.1 - p.5" onChange={e=>setPages(e.target.value)} />
+            <input type={'text'} className="m-4" placeholder="p.1 - p.5" onChange={e=>setPages(tify(e.target.value))} value={pages}/>
         </div>
     )
 }
--- pages/editor/index.jsx
+++ pages/editor/index.jsx
@@ -8,6 +8,8 @@ import Des from "./components/Des";
 import Submit from "./components/Submit";
 import View from "./components/View";
 import { GOOGLE_SHEET_URL } from "../../env/config"
+import moment from "moment";
+import { change, howManyDaysLeft } from "./public/change";
 
 export default function Editor() {
 
@@ -19,18 +21,83 @@ export default function Editor() {
     let [des, setDes] = useState('');
     let clickRef = useRef(false);
     let [clickStatus, setClickStatus] = useState(0);
+    let [alert, setAlert] = useState(<div></div>);
+
+    function clean() {
+        setSubject([]);
+        setDoing('');
+        setBook('');
+        setDate('');
+        setPages('');
+        setDes('');
+    }
+
+    let todayYear = moment().format('YYYY')
+    let todayMonth = moment().format('MM')
+    let todayDate = moment().format('DD')
+    let todayDay = moment().format('d')
+
+    let DateDay = moment(date, "YYYY[-]MM[-]DD").format('d')//執行時間的星期
+
+    let spendDay = moment(`${todayYear}-${todayMonth}-${todayDate}`).to(date)
+
+    useEffect(() => {
+
+        if (spendDay == 'a few seconds ago') {
+            setDate('今天')
+        } else if (spendDay == 'in a day') {
+            setDate('明天')
+        } else if (spendDay == 'in 2 days') {
+            setDate('後天')
+        } else if (spendDay == 'in 7 days') {
+            setDate('下禮拜')
+        } else {
+            spendDay = spendDay.replace('in', '')
+            spendDay = spendDay.replace('days', '')
+            spendDay = Number(spendDay)
+            console.log(spendDay);
+            if (howManyDaysLeft(todayDay) + 7 > spendDay) {
+                if (howManyDaysLeft(todayDay) <= spendDay) {
+                    setDate('下星期' + change(DateDay))
+                } else {
+                    setDate('星期' + change(DateDay))
+                }
+
+            }
+
+        }
+
+    }, [date])
+
+
 
 
     useEffect(() => {
 
-        if (!clickRef.current) {
+        if (!clickRef.current || !pages) {
             clickRef.current = true
         } else {
-            let fetchUrl = `${GOOGLE_SHEET_URL}?year=2022&month=02&datetoday=28&day=${1}&subject=${subject}&action=${doing}&book=${book}&date=${date}&pages=${pages}&des=${des}&password=mouse0609`
+            let fetchUrl = `${GOOGLE_SHEET_URL}?year=${todayYear}&month=${todayMonth}&datetoday=${todayDate}&day=${todayDay}&subject=${subject}&action=${doing}&book=${book}&date=${date}&pages=${pages}&des=${des}&password=mouse0609`
             fetch(fetchUrl).then((res) => {
+                clean()
                 return res.status
-            }).then((status)=>{
-                alert(status==200 ? "成功" : "失敗")
+            }).then((status) => {
+                if (status == 200) {
+                    setAlert(
+                        <div className="alert alert-success" role="alert">
+                            <p>SUCCESS!!! It will be disappear few second later</p>
+                        </div>
+                    )
+                } else {
+                    setAlert(
+                        <div className="alert alert-danger" role="alert">
+                            <p>Oops!!! Please try again.</p>
+                        </div>
+                    )
+                }
+                setTimeout(()=>{
+                    setAlert(<div></div>);
+                }, 2000)
             })
 
         }
@@ -38,13 +105,14 @@ export default function Editor() {
 
     return (
         <div className="container p-5">
+            {alert}
             <View subject={subject} doing={doing} book={book} date={date} pages={pages} des={des} />
             <Subject setSubject={setSubject} subject={subject} />
             <Doing setDoing={setDoing} />
             <Book setBook={setBook} />
             <Date setDate={setDate} />
-            <Page setPages={setPages} />
-            <Des setDes={setDes} />
+            <Page setPages={setPages} pages={pages} />
+            <Des setDes={setDes} des={des} />
             <Submit setClickStatus={setClickStatus} />
         </div>
     )
