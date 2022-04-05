0cff9c05c0b8885ec18629f00b94dfad29793b45
letterpaulsand <letterpaulsand@gmail.com>
2022-04-05 15:08:37 +0800

    fix the sign up

--- env/config.js
+++ env/config.js
@@ -1,4 +1,4 @@
 const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbymMZ9kdfUye5EFOrl00L_Mnysf_eQYltLovWiGqH4hBPhLc60YW-6I4t3KCshyvZny/exec"
 const BLOG_URL = "https://script.google.com/macros/s/AKfycbwFOReLNSDx5zo3EUOK00x84DDzs489iBqvK9jXPpwPxKEhcARGBSjs9UmRFZ453ZQy/exec"
-const LOGIN = "https://script.google.com/macros/s/AKfycbxH5ycy9feGHep5JnobBtoE-nggi_PB2DN8NGA1L8S2tatN_kHetEjQ-UDkzUpuU7Xdsg/exec"
+const LOGIN = "https://script.google.com/macros/s/AKfycbxX5cdQJLKyaj_wmI5iSX1ss9PGOJyMpYz60SHjaEOdZn9aUw58emrwX4TWzQSbLj4y4g/exec"
 export {GOOGLE_SHEET_URL, BLOG_URL, LOGIN}
