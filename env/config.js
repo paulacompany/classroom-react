ab43cfed5b32a0bbdb37aaf447d3074650f29680
letterpaulsand <letterpaulsand@gmail.com>
2022-04-06 23:50:53 +0800

    fix the sign up

--- env/config.js
+++ env/config.js
@@ -1,4 +1,4 @@
 const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbymMZ9kdfUye5EFOrl00L_Mnysf_eQYltLovWiGqH4hBPhLc60YW-6I4t3KCshyvZny/exec"
 const BLOG_URL = "https://script.google.com/macros/s/AKfycbwFOReLNSDx5zo3EUOK00x84DDzs489iBqvK9jXPpwPxKEhcARGBSjs9UmRFZ453ZQy/exec"
-const LOGIN = "https://script.google.com/macros/s/AKfycbxX5cdQJLKyaj_wmI5iSX1ss9PGOJyMpYz60SHjaEOdZn9aUw58emrwX4TWzQSbLj4y4g/exec"
+const LOGIN = "https://script.google.com/macros/s/AKfycbzyVZLhl2WgStY3D_1DeUrzLYxrkVthHZQzhEJRBragAZ_ayZK9p8SZwpRl11Oc92EXzQ/exec"
 export {GOOGLE_SHEET_URL, BLOG_URL, LOGIN}
--- pages/login/signup.jsx
+++ pages/login/signup.jsx
@@ -3,39 +3,81 @@ import Link from "next/link";
 import { useRouter } from 'next/router'
 import { LOGIN } from "../../env/config";
 
-export default function Signup(){
+export default function Signup() {
 
     let router = useRouter()
 
     let [email, setEmail] = useState('')
     let [password, setPassword] = useState('')
+    let [checkItem, setCheckItem] = useState(false);
+    let [checkNumber, setCheckNumber] = useState();
+    let [clickEnter, setClickEnter] = useState(false)
 
-    function click(){
-        fetch(`${LOGIN}?email=${email}&password=${password}&mode=sign`).then(res=>{
+    function click() {
+        fetch(`${LOGIN}?email=${email}&password=${password}&mode=sign`).then(res => {
             return res.text()
-        }).then(data=>{
-            if(data == 'success'){
-                alert('Please check your email')
-                router.push('/')
+        }).then(data => {
+            if (data == 'success') {
+                setCheckItem(true)
+        
+                    
+                
+                
+            } else {
+                alert('error')
             }
         })
     }
 
-    return(
+    function checkDom() {
+        if (checkItem) {
+            return (
+                <>
+                    <p className="h3 m-2 mt-5">conform code:</p>
+                    <input className="form-control mb-5" type={'number'} onKeyUp={e => {
+                        setCheckNumber(e.target.value)
+                        if(e.key == 'Enter'){
+                            checkFetch(email, password, checkNumber)
+                        }
+                    }} />
+                </>
+            )
+        } else {
+            return ''
+        }
+    }
+
+    function checkFetch(email, password, code) {
+        fetch(`${LOGIN}?email=${email}&password=${password}&mode=conform&number=${code}`).then(res => {
+            return res.text()
+        }).then(data => {
+            console.log(email, password, code);
+            if (data == 'ok') {
+                router.push('/')
+            }else if (data == 'you check it before') {
+                alert(data)
+            } else {
+                alert(`${data}. Please sign up again.`)
+            }
+        })
+    }
+
+    return (
         <div className="container mh-100 login-container">
             <h1 className="m-5">Sign up</h1>
             <p className="h3 m-2">email:</p>
-            <input type={'email'} className="form-control mb-5" onChange={e=>{
+            <input type={'email'} className="form-control mb-5" onChange={e => {
                 setEmail(e.target.value)
             }} />
             <p className="h3 m-2">password:</p>
-            <input type={'password'} className="form-control mb-5" onChange={e=>{
+            <input type={'password'} className="form-control mb-5" onChange={e => {
                 setPassword(e.target.value)
             }} />
             <div className="w-100">
                 <button className="btn btn-danger mx-2 btn-lg" onClick={click}>sign up</button>
                 <Link href={'/login'}><button className="btn btn-success mx-2">login</button></Link>
             </div>
+            {checkDom()}
         </div>
     )
 }
